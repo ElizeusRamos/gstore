4b5d8f6ce96aad99f2ba123f674805a421c8d71b
Elizeu Ramos <elizeu.nascimento@autbank.com.br>
2022-06-19 16:19:47 -0300

    feat/4 - Commit final antes da entrega 2022-06-20

--- GStoreWorkspace/GStoreWeb/src/com/gstore/servlets/CategoriaController.java
+++ GStoreWorkspace/GStoreWeb/src/com/gstore/servlets/CategoriaController.java
@@ -7,15 +7,66 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.gstore.dao.CategoriaDAO;
+import com.gstore.model.Categoria;
+
 @WebServlet("/CategoriaController")
 public class CategoriaController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private CategoriaDAO cdao = new CategoriaDAO();
        
     public CategoriaController() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+String action = request.getParameter("action");
+		
+		String codigoString = request.getParameter("id");
+		String nome = request.getParameter("nome");
+		String descricao = request.getParameter("descricao");
+		String ativoString = request.getParameter("ativo");
+		
+		Categoria categoria = new Categoria();
+		
+		if(action == null) { 
+			action = "read";
+		}
+		
+		if(codigoString != null) {
+			int codigo = Integer.parseInt(codigoString);
+			categoria.setCodigo(codigo);
+			System.err.println("Código do cargo: " + categoria.getCodigo());
+		}
+		
+		if(ativoString != null) {
+			boolean ativo = Boolean.parseBoolean(ativoString);
+			categoria.setAtivo(ativo);
+			System.err.println("Status do cargo: " + categoria.isAtivo());
+		}
+		
+		categoria.setNome(nome);
+		categoria.setDescricao(descricao);
+		
+		switch (action) {
+		case "create":
+			cdao.create(categoria);
+			System.err.println("Categoria Adicionada com Sucesso!");
+			response.sendRedirect("/GStoreWeb/CategoriaController"); 	
+			break;
+		case "read":
+			request.setAttribute("lista", cdao.list()); 				// devolver a lista de cargos
+			System.err.println("Listagem de Categorias completa!");
+			request.getRequestDispatcher("/WEB-INF/Categoria.jsp").forward(request, response);
+			break;
+		case "delete":
+			cdao.delete(categoria.getCodigo());
+			System.err.println("Cargo Removido!");
+			response.sendRedirect("/GStoreWeb/CategoriaController"); 
+			break;
+		default:
+			break;
+		}
 		
 	}
 
